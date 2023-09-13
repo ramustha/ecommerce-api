2e0c5ce0e8c5f3d4efcbffd23b12ac43dfadc2d4
Ramustha <ramusthanest@gmail.com>
2023-09-13 17:00:25 +0700

    "Added web scraping feature using Puppeteer & Playwright
    App.js was created to handle both Puppeteer and Playwright web scraping mechanisms for the scraper-engine. The file implements two async functions for both solutions, including appropriate handling for errors and timeouts. Package.json has added dependencies required for the scraper feature."
    
    Package-lock.json has been updated with the necessary dependencies for the scraper feature. Changes allow the application to scrape data from websites using Puppeteer or Playwright, chosen based on the user's decision."

---
+++ scraper-engine/app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const puppeteer = require('puppeteer-extra')
+const {chromium} = require('playwright-extra')
+const StealthPlugin = require("puppeteer-extra-plugin-stealth")
+const app = express()
+const port = 3000
+const defaultTimeout = 0
+
+async function performScrapingPuppeteer(url, timeout, res) {
+    puppeteer.use(StealthPlugin())
+
+    const browser = await puppeteer.launch({
+        headless: 'new',
+        defaultViewport: null,
+    });
+    const page = await browser.newPage();
+
+    try {
+        console.log('Scraping puppeteer..' + url);
+        await page.goto(url, {waitUntil: 'networkidle0'})
+        await page.setDefaultTimeout(timeout)
+        // await page.screenshot({path: 'puppeteer-stealth.png', fullPage: true})
+
+        console.log('All done ✨');
+        const content = await page.content();
+        await browser.close();
+
+        res.send(content);
+    } catch (err) {
+        await browser.close();
+        console.error(err);
+        res.status(500).send('An error occurred while trying to scrape the website.');
+    }
+}
+
+async function performScrapingPlaywright(url, timeout, res) {
+    chromium.use(StealthPlugin());
+
+    const browser = await chromium.launch({headless: true});
+    const page = await browser.newPage()
+
+    try {
+        console.log('Scraping playwright..' + url);
+        await page.goto(url, {waitUntil: 'networkidle', timeout: timeout})
+        // await page.screenshot({path: 'playwright-stealth.png', fullPage: true})
+
+        console.log('All done ✨');
+        const content = await page.content();
+        await browser.close();
+
+        res.send(content);
+    } catch (err) {
+        await browser.close();
+        console.error(err);
+        res.status(500).send('An error occurred while trying to scrape the website.');
+    }
+}
+
+app.get('/', async (req, res) => {
+    let {url, timeout, mode} = req.query;
+
+    if (!url) {
+        return res.status(400).send('The url parameter is required.');
+    }
+
+    timeout = timeout ? parseInt(timeout) : defaultTimeout;
+    if (mode === '1')
+        await performScrapingPuppeteer(url, timeout, res);
+    else
+        await performScrapingPlaywright(url, timeout, res);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
